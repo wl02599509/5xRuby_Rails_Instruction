9a924a4829b1404d2dea3b1edd1b69f8957b9a57
Mike <wl02599509@gmail.com>
2022-07-27 17:00:42 +0800

    add blog index new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: "welcome#home"
+  get '/about', to: "welcome#about"
+
+  get '/blog', to: "blog#index"
+  get '/blog/new', to: "blog#new"
+  post '/blog', to: "blog#create"
 end
