54e8ef7c06fd510e1326ec6aac4c3b29e1350e50
Mike <wl02599509@gmail.com>
2022-08-01 16:08:37 +0800

    rescue_from in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_fund
+
+	def not_fund
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
 end
