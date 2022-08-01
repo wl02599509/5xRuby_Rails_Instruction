63cfc685c7328b5d11c584173f9cd629a8c42d60
Mike <wl02599509@gmail.com>
2022-08-01 17:17:12 +0800

    整理views，並練習 partial render

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,9 @@ class BlogsController < ApplicationController
   
   def index
     @articles = Article.all
+    @ad_color_red = "Red"
+    @ad_color_green = "green"
+    @ad_color_orange = "orange"
   end
 
   def new
