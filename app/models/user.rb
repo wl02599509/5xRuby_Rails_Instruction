3413ec00a1af0007b89e385dddd1a0824a0b0b2e
Mike <wl02599509@gmail.com>
2022-08-03 15:30:36 +0800

    增加驗證與加密

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  validates :email, 
+             presence: true,
+             uniqueness: true
+
+  validates :password, 
+             presence: true, 
+             length: { minimum: 6 },
+             confirmation: true
+
+  #callback
+  before_create :encrypt_password
+
+  def encrypt_password
+    # require "digest" # Rails 環境內已經有 "digest" 了
+    pw = "xx-#{self.password}-yy"
+    self.password = Digest::SHA1.hexdigest(pw)
+  end
+  
 end
