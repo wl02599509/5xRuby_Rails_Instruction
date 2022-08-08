469e7c86dc895439b24b4cb2ae98cdc0ce1b61be
Mike <wl02599509@gmail.com>
2022-08-08 12:15:46 +0800

    add new routes: unlock

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/about', to: "welcome#about"
 
   resources :blogs
-  resources :articles
+  resources :articles do
+    member do
+      patch :unlock
+    end 
+  end
   resource :sessions, only: [:create, :destroy]
 
   resource :users, except: [:destroy, :new] do
