d0d0c19015e059d84ada6d2ca317b10244f2466d
Eddie <eddie@5xcampus.com>
2022-08-17 16:06:15 +0800

    add state machine to order

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'paranoia', '~> 2.6'
 gem "kaminari", "~> 1.2"
+gem "aasm", "~> 5.3"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -58,4 +59,3 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,29 @@
 class Order < ApplicationRecord
+  include AASM
+
   belongs_to :user
 
   validates :serial, :price, :state, presence: true
+
+  aasm column: "state", no_direct_assignment: true do
+    state :pending, initial: true
+    state :paid, :refunded, :cancelled, :fail
+
+    event :pay do
+      transitions from: [:pending, :fail], to: :paid
+    end
+
+    event :fail do
+      transitions from: :pending, to: :fail
+    end
+
+    event :cancel do
+      transitions from: [:pending, :fail] , to: :cancelled
+    end
+
+    event :refund do
+      transitions from: :paid, to: :refunded
+    end
+  end
+
 end
